ca0e7716f125051c8eb4b5f8ae7e3bacf098a2c1
phone-myintmyat <hibana0055@gmail.com>
2026-09-14 15:37:55 +0630

    Added new line in testing

---
+++ src/main/java/com/napier/sem/Testing.java
@@ -0,0 +1,9 @@
+package com.napier.sem;
+
+public class Testing {
+    public static void main(String[] args)
+    {
+        System.out.println("This is a testing file for git");
+        System.out.println("Hi Hello How are you");
+    }
+}
